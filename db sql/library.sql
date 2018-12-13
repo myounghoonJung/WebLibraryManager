ed7ebd94062df4e4bcd87b60f17be36ffe3012ca
MyoungHoon Jung <mh.jung.dev@gmail.com>
2018-12-13 11:16:05 +0900

    Update library.sql

--- db sql/library.sql
+++ db sql/library.sql
@@ -4,21 +4,20 @@
 -- table 생성
 
 create table tbl_book(
-    bookid number constraint pk_book_id primary key,
+    bookid varchar2(60) constraint pk_book_id primary key,
     booktitle varchar2(60) not null,
     author varchar2(60) not null,
     genre varchar2(30) not null,
-    status char(1) default 'N' check (status in ('B','N')) not null,
-    delflag char(1) default 'N' check (delflag in ('Y','N')) not null,
-    deldate date
+    status char(1) default 'N' check (status in ('B','N')) not null
 );
 
 create table tbl_book_del(
-    bookid number constraint pk_book_del_id primary key,
+    delno number constraint pk_book_del_delno primary key,
+    deldate date default sysdate not null,
+    bookid varchar2(60) not null,
     booktitle varchar2(60) not null,
     author varchar2(60) not null,
-    genre varchar2(30) not null,
-    deldate date default sysdate not null
+    genre varchar2(30) not null
 );
 
 create table tbl_member(
@@ -31,13 +30,13 @@ create table tbl_member(
     favoritegenre varchar2(60) not null,
     presentborrowcount number default 0,
     historyborrowcount number default 0,
-    enrolldate date default sysdate not null,
-    quitflag char(1) default 'N' check (quitflag in ('Y','N')) not null,
-    quitdate date
+    enrolldate date default sysdate not null
 );
 
 create table tbl_member_quit(
-    memberid varchar2(30) constraint pk_member_quit_memberid primary key,
+    quitno number constraint pk_member_quit_quitno primary key,
+    quitdate date default sysdate not null,
+    memberid varchar2(30) not null,
     memberpw varchar2(30) not null,
     membername varchar2(20) not null,
     gender char(1) check (gender in ('M','F')) not null,
@@ -45,22 +44,18 @@ create table tbl_member_quit(
     phone varchar2(13) not null,
     favoritegenre varchar2(60) not null,
     historyborrowcount number not null,
-    enrolldate date not null,
-    quitdate date default sysdate not null
+    enrolldate date not null
 );
 
 create table tbl_borrow_logger(
     logno number constraint pk_borrow_logno primary key,
     logdate date default sysdate not null,
-    bookid number constraint fk_borrow_bookid references tbl_book(bookid),
-    booktitle varchar2(60) not null,
-    author varchar2(60) not null,
-    genre varchar2(30) not null,
+    bookid varchar2(60) not null,
     borrowdate date,
     untilborrowdate date,
     returndate date,
     status char(1) default 'N' check (status in ('B','R')) not null,
-    borrowmemberid constraint fk_borrow_memberid references tbl_member(memberid)
+    memberid varchar2(30) not null
 );
 
 create table tbl_member_logger(
@@ -73,12 +68,14 @@ create table tbl_member_logger(
 
 
 -- sequence 생성
+-- tbl_book_del(delno)
+create sequence seq_book_del;
 
--- tbl_book(bookid)
-create sequence seq_book;
+-- tbl_member_quit(quitno)
+create sequence seq_member_quit;
 
--- tbl_borrow_log(logno)
-create sequence seq_borrow_log;
+-- tbl_borrow_logger(logno)
+create sequence seq_borrow_logger;
 
 -- tbl_member_logger(logno)
 create sequence seq_member_logger;
@@ -92,45 +89,44 @@ create or replace trigger trg_borrow_logger
     insert on tbl_borrow_logger
     for each row
 declare
-    anotherbookid number;
-    anotherstatus char(1);
+    cnt_memberid varchar2(30);
+    cnt_bookid varchar2(60);
+    v_status char(1);
 begin
-    anotherbookid := :new.bookid;
-    select status into anotherstatus from tbl_book where bookid=anotherbookid;
+    select status into v_status from tbl_book where bookid=:new.bookid;
+    -- case문으로 bookid랑 memberid가 존재해야지만 빌릴 수 있게
+    select count(*) into cnt_memberid from tbl_member where memberid=:new.memberid;
+    select count(*) into cnt_bookid from tbl_book where bookid=:new.bookid;
     case
-        when :new.status='B' and anotherstatus = 'N' then
+        when :new.status='B' and v_status = 'N' and cnt_memberid = 1 and cnt_bookid = 1 then
             update tbl_book set status='B' where bookid=:new.bookid;
-            update tbl_member set presentborrowcount=(select presentborrowcount from tbl_member)+1;
-            update tbl_member set historyborrowcount=(select historyborrowcount from tbl_member)+1;
-        when :new.status='R' and anotherstatus = 'B' then
+            update tbl_member set presentborrowcount=(select presentborrowcount from tbl_member)+1 where memberid=:new.memberid;
+            update tbl_member set historyborrowcount=(select historyborrowcount from tbl_member)+1 where memberid=:new.memberid;
+        when :new.status='R' and v_status = 'B' and cnt_memberid = 1 and cnt_bookid = 1 then
             update tbl_book set status='N' where bookid=:new.bookid;
-            update tbl_member set presentborrowcount=(select presentborrowcount from tbl_member)-1;
+            update tbl_member set presentborrowcount=(select presentborrowcount from tbl_member)-1 where memberid=:new.memberid;
         end case;
 end;
 /
 
--- 도서 삭제(delflag 변경)하면 tbl_book_del에 추가
+-- 도서 삭제하면 tbl_book_del에 추가
 create or replace trigger trg_book_del
     after
-    update of delflag, deldate on tbl_book
+    delete on tbl_book
     for each row
 begin
-    case when :new.delflag='Y' then
-        insert into tbl_book_del (bookid,booktitle,author,genre) values (:old.bookid,:old.booktitle,:old.author,:old.genre);
-    end case;
+    insert into tbl_book_del (delno,bookid,booktitle,author,genre) values (SEQ_BOOK_DEL.nextval,:old.bookid,:old.booktitle,:old.author,:old.genre);
 end;
 /
 
--- 멤버 삭제(quitflag 변경)하면 tbl_member_quit에 추가
+-- 멤버 삭제하면 tbl_member_quit에 추가
 create or replace trigger trg_member_quit
     after
-    update of quitflag, quitdate on tbl_member
+    delete on tbl_member
     for each row
 begin
-    case when :new.quitflag='Y' then
-        insert into tbl_member_quit (memberid,memberpw,membername,gender,birthday,phone,favoritegenre,historyborrowcount,enrolldate) 
-                             values (:old.memberid,:old.memberpw,:old.membername,:old.gender,:old.birthday,:old.phone,:old.favoritegenre,:old.historyborrowcount,:old.enrolldate);
-    end case;
+    insert into tbl_member_quit (quitno,memberid,memberpw,membername,gender,birthday,phone,favoritegenre,historyborrowcount,enrolldate) 
+                         values (seq_member_quit.nextval,:old.memberid,:old.memberpw,:old.membername,:old.gender,:old.birthday,:old.phone,:old.favoritegenre,:old.historyborrowcount,:old.enrolldate);
 end;
 /
 
